2422a8a05c021e6aff37f1ad5b09aced1c16b753
DESKTOP-9GVJBJP\Reishi <sebastian.kubrycz@gmail.com>
2025-02-05 19:41:45 +0100

    feature: Added movement for circles and canvas window boundaries collision detection

--- client/src/components/Canvas/Canvas.vue
+++ client/src/components/Canvas/Canvas.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
+import { onMounted, ref } from 'vue'
 
 interface Circle {
+  id: number
   x: number
   y: number
   r: number
   color: Color
+  direction: Direction
 }
 
 interface Color {
@@ -15,14 +17,21 @@ interface Color {
   a: number
 }
 
+interface Direction {
+  dx: number
+  dy: number
+}
+
 const width = ref(window.innerWidth)
 const height = ref(window.innerHeight)
 
 const canvas = ref<HTMLCanvasElement | null>(null)
 const circles = ref<Circle[]>([])
+let circlesCount = 0
+const speedMultiplier = 15
 
 let previousDelta = 0
-const fps = 24
+const fps = 30
 
 const drawRect = (ctx: CanvasRenderingContext2D) => {
   ctx.fillStyle = 'transparent'
@@ -30,13 +39,20 @@ const drawRect = (ctx: CanvasRenderingContext2D) => {
 }
 
 const createCircle = (x: number, y: number, r: number, color: Color) => {
+  const direction: Direction = {
+    dx: Math.random() * 2 - 1,
+    dy: Math.random() * 2 - 1,
+  }
   const circle: Circle = {
+    id: circlesCount,
     x: x,
     y: y,
     r: r,
     color: color,
+    direction: direction,
   }
   circles.value.push(circle)
+  circlesCount++
 }
 
 const drawRadialGradient = (
@@ -51,6 +67,30 @@ const drawRadialGradient = (
   gradient.addColorStop(1, `rgba(${color.r}, ${color.g}, ${color.b}, 0.0)`)
   ctx.fillStyle = gradient
   ctx.fillRect(0, 0, width.value, height.value)
+}
+
+const checkCollision = (x: number, y: number, dx: number, dy: number) => {
+  let newX = x
+  let newY = y
+  let newDx = dx
+  let newDy = dy
+
+  if (x > width.value - speedMultiplier) {
+    newX = x - speedMultiplier
+    newDx = -1 * dx
+  } else if (x < speedMultiplier) {
+    newX = speedMultiplier
+    newDx = -1 * dx
+  }
+  if (y > height.value - speedMultiplier) {
+    newY = y - speedMultiplier
+    newDy = -1 * dy
+  } else if (y < speedMultiplier) {
+    newY = speedMultiplier
+    newDy = -1 * dy
+  }
+
+  return [newX, newY, newDx, newDy]
 }
 
 const draw = () => {
@@ -75,24 +115,31 @@ const draw = () => {
 }
 
 onMounted(() => {
-  for (let i = 0; i < 5; i++) {
-    const r = 600 + i
+  for (let i = 0; i < 20; i++) {
+    const r = 300
     const x = Math.floor(Math.random() * width.value)
     const y = Math.floor(Math.random() * height.value)
     const color: Color = {
-      r: Math.floor(Math.random() * 255),
-      g: Math.floor(Math.random() * 255),
-      b: Math.floor(Math.random() * 255),
+      r: Math.floor(Math.random() * 60),
+      g: Math.floor(Math.random() * 60),
+      b: 100 + Math.floor(Math.random() * 50),
       a: 1.0,
     }
     createCircle(x, y, r, color)
   }
 
   const interval = setInterval(() => {
-    const multiplier = 10
     circles.value.forEach((el) => {
-      el.x += 1 * multiplier - Math.random() * 2 * multiplier
-      el.y += 1 * multiplier - Math.random() * 2 * multiplier
+      const { direction } = el
+      const { dx, dy } = direction
+      const arr = checkCollision(el.x, el.y, dx, dy)
+      el.x = arr[0] //1 * multiplier - Math.random() * 2 * multiplier
+      el.y = arr[1] //1 * multiplier - Math.random() * 2 * multiplier
+      el.direction.dx = arr[2]
+      el.direction.dy = arr[3]
+
+      el.x += el.direction.dx * speedMultiplier
+      el.y += el.direction.dy * speedMultiplier
     })
   }, 1000 / fps)
 
