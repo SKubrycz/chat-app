8a9398d9d11d74807cb21311abe4d91447dbd519
DESKTOP-9GVJBJP\Reishi <sebastian.kubrycz@gmail.com>
2025-02-12 22:49:54 +0100

    refactor: Adjusted Login form and Toast event emit

--- client/src/components/Login/Login.vue
+++ client/src/components/Login/Login.vue
@@ -5,13 +5,33 @@ import { Form } from '@primevue/forms'
 
 const emit = defineEmits(['showToast'])
 
-const handleToast = () => {
-  emit('showToast', {
-    severity: 'info',
-    summary: 'Information',
-    detail: 'MEssage somfweaor',
-    life: 3000,
-  })
+const handleToast = (severity: string, status: number, message: string) => {
+  if (status < 400) {
+    emit('showToast', {
+      severity: severity,
+      summary: 'Information',
+      detail: message,
+      life: 3000,
+    })
+  } else if (status >= 300 && status < 400) {
+    emit('showToast', {
+      severity: severity,
+      summary: 'Warning',
+      detail: message,
+      life: 3000,
+    })
+  } else if (status >= 400) {
+    emit('showToast', {
+      severity: severity,
+      summary: 'Error',
+      detail: message,
+      life: 3000,
+    })
+  }
+}
+
+const onFormSubmit = () => {
+  handleToast('info', 200, 'Hello') // test
 }
 </script>
 
@@ -19,11 +39,11 @@ const handleToast = () => {
   <Canvas></Canvas>
   <div class="center-col come-up">
     <h1><RouterLink to="/">Chat app</RouterLink></h1>
-    <Form>
+    <Form v-slot="$form" @submit="onFormSubmit">
       <div class="center-col">
         <InputText name="username" type="text" placeholder="Username" fluid required></InputText>
         <Password placeholder="Password" :feedback="false" toggle-mask fluid required></Password>
-        <Button type="submit" label="Login" @click="handleToast()"></Button>
+        <Button type="submit" label="Login"></Button>
       </div>
     </Form>
     <div>go to <RouterLink to="/register">Register</RouterLink></div>
