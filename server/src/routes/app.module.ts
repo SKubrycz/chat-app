a65e355b3c6424b67854f37b9f10b37d5d48679e
DESKTOP-9GVJBJP\Reishi <sebastian.kubrycz@gmail.com>
2025-02-28 11:02:32 +0100

    feature: Set up a connection to Docker Mongo database container

--- server/src/routes/app.module.ts
+++ server/src/routes/app.module.ts
@@ -6,10 +6,33 @@ import { RegisterModule } from "./register/register.module";
 import { APP_FILTER, APP_PIPE } from "@nestjs/core";
 import { HttpExceptionFilter } from "src/httpException.filter";
 import { ChatModule } from "./chat/chat.module";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Connection } from "mongoose";
 
 @Module({
-  imports: [ConfigModule.forRoot(), LoginModule, RegisterModule, ChatModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>("DB_URL"),
+        onConnectionCreate: (connection: Connection) => {
+          connection.on("connected", () =>
+            console.log(
+              "\x1b[92m\x1b[1m" + "[MongoDB] => connected" + "\x1b[0m"
+            )
+          );
+
+          return connection;
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    LoginModule,
+    RegisterModule,
+    ChatModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
